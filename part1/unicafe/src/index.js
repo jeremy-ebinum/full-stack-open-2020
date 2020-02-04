1474d622e569074d893fd51fa2f19c6938cafa42
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-04 07:29:51 +0100

    wip(part1/unicafe): Initial commit
    - Create project environment with create-react-App
    - Add starter code to index.js

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
