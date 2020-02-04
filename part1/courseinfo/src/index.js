545fafdd58109b3a49437b459cd99db0da6b4e8a
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-04 03:26:21 +0100

    refactor(part1/courseinfo): Split up App rendering
    - Create Header, Content, Part and Footer components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,30 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = props => {
+  return (
+    <p>
+      {props.name} {props.exCount}
+    </p>
+  );
+};
+
+const Content = props => {
+  return (
+    <div>
+      <Part name={props.name1} exCount={props.exCount1} />
+      <Part name={props.name2} exCount={props.exCount2} />
+      <Part name={props.name3} exCount={props.exCount3} />
+    </div>
+  );
+};
+
+const Total = props => {
+  return <p>Number of excercises {props.num1 + props.num2 + props.num3}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        name1={part1}
+        exCount1={exercises1}
+        name2={part2}
+        exCount2={exercises2}
+        name3={part3}
+        exCount3={exercises3}
+      />
+      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
