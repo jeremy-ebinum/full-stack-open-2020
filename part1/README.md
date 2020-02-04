adb25850b3681c36a0c361bd8855f0b7d81cf42a
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-04 02:58:17 +0100

    wip(part1/courseinfo): Initial commit
    - Create project environment with create-react-app
    - Add initial root code to index.js

---
+++ part1/README.md
@@ -0,0 +1,3 @@
+# Full Stack Open 2019 - Excercise Solutions
+
+## Part 1 - [Introduction to React](https://fullstackopen.com/en/part1)
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
