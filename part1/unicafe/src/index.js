c123e9ccc2d2fa1d50c496dce721785477afa0c3
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-04 09:38:39 +0100

    feat(part1/unicafe): Only display statistics when there is feedback

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,6 +6,10 @@ const Heading = props => <h1>{props.text}</h1>;
 const Button = props => <button onClick={props.onClick}>{props.text}</button>;
 
 const Statistics = props => {
+  if (!props.hasFeedback) {
+    return <p>No feedback has been given</p>;
+  }
+
   return (
     <div>
       <Heading text="Statistics" />
@@ -30,6 +34,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [hasFeedback, setHasFeedback] = useState(false);
 
   const feedback = [
     { name: "good", count: good },
@@ -70,14 +75,19 @@ const App = () => {
     total
   );
 
-  const handleFeedbackButtonClick = type => {
+  const handleButtonClick = type => {
+    setHasFeedback(true);
+
     switch (type) {
       case "good":
-        return () => setGood(good + 1);
+        setGood(good + 1);
+        break;
       case "neutral":
-        return () => setNeutral(neutral + 1);
+        setNeutral(neutral + 1);
+        break;
       case "bad":
-        return () => setBad(bad + 1);
+        setBad(bad + 1);
+        break;
       default:
         break;
     }
@@ -86,12 +96,13 @@ const App = () => {
   return (
     <div>
       <Heading text="Give Feedback" />
-      <Button onClick={handleFeedbackButtonClick("good")} text="good" />
-      <Button onClick={handleFeedbackButtonClick("neutral")} text="neutral" />
-      <Button onClick={handleFeedbackButtonClick("bad")} text="bad" />
+      <Button onClick={() => handleButtonClick("good")} text="good" />
+      <Button onClick={() => handleButtonClick("neutral")} text="neutral" />
+      <Button onClick={() => handleButtonClick("bad")} text="bad" />
       <Statistics
-        feedback={feedback}
+        hasFeedback={hasFeedback}
         total={total}
+        feedback={feedback}
         averageScore={averageScore}
         percentPositive={percentPositive}
       />
