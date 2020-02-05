0eda60923e9bca9761e5ddb1154acdd4217cbffb
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-05 04:25:06 +0100

    wip(part2/phonebook): Initial commit
    - Bootstrap create-react-app project
    - Add starter code to App.js

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
+
+export default App;
