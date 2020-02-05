8170c5d222d47b25d8f46555dd078aab6f0e84ba
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-05 04:45:31 +0100

    feat(part2/phonebook): Add new names from user input

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
 
+const Person = props => {
+  return (
+    <>
+      {props.name}
+      <br />
+    </>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const personsList = persons.map(person => {
+    return <Person key={person.name} name={person.name} />;
+  });
+
+  const handleChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const person = {
+      name: newName
+    };
+
+    setPersons([...persons].concat(person));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={event => handleSubmit(event)}>
         <div>
-          name: <input />
+          name:{" "}
+          <input onChange={event => handleChange(event)} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {personsList}
     </div>
   );
 };
