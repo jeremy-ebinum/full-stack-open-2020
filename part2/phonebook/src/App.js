90eaf00e12ece8a8725c99a9fe3a3e3e6820490e
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-05 05:42:07 +0100

    feat(part2/phonebook): Add new numbers from user input

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,25 +3,60 @@ import React, { useState } from "react";
 const Person = props => {
   return (
     <>
-      {props.name}
+      {props.name} {props.number}
       <br />
     </>
   );
 };
 
+const initialPersons = [
+  { name: "Arto Hellas", number: "040-123456" },
+  { name: "Ada Lovelace", number: "39-44-5323523" },
+  { name: "Dan Abramov", number: "12-43-234345" },
+  { name: "Mary Poppendieck", number: "39-23-6423122" }
+];
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState(initialPersons);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
   const names = persons.map(person => {
     return person.name.toLocaleLowerCase();
   });
 
   const personsList = persons.map(person => {
-    return <Person key={person.name} name={person.name} />;
+    return (
+      <Person key={person.name} name={person.name} number={person.number} />
+    );
   });
 
-  const handleChange = event => {
-    setNewName(event.target.value);
+  const handleChange = (event, type) => {
+    switch (type) {
+      case "name":
+        setNewName(event.target.value);
+        break;
+      case "number":
+        setNewNumber(event.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const createNewPerson = () => {
+    const replacer = (match, p1, p2) => {
+      return p1.toUpperCase() + p2.toLocaleLowerCase();
+    };
+
+    const titleCasedName = newName.replace(/\b([a-zA-Z])(\w+)/g, replacer);
+
+    const person = {
+      name: titleCasedName,
+      number: newNumber
+    };
+
+    return person;
   };
 
   const handleSubmit = event => {
@@ -30,20 +65,12 @@ const App = () => {
     if (names.includes(newName.toLocaleLowerCase())) {
       alert(`${newName} is already added to the phonebook.`);
     } else {
-      const replacer = (match, p1, p2) => {
-        return p1.toUpperCase() + p2.toLocaleLowerCase();
-      };
-
-      const titleCasedName = newName.replace(/\b([a-zA-Z])(\w+)/g, replacer);
-
-      const person = {
-        name: titleCasedName
-      };
-
+      const person = createNewPerson();
       setPersons([...persons].concat(person));
     }
 
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -51,11 +78,21 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={event => handleSubmit(event)}>
         <div>
-          name:{" "}
-          <input onChange={event => handleChange(event)} value={newName} />
+          Name:{" "}
+          <input
+            onChange={event => handleChange(event, "name")}
+            value={newName}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          Number:{" "}
+          <input
+            onChange={event => handleChange(event, "number")}
+            value={newNumber}
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
