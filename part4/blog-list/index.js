64cebb0ba334a59982d18ba3e60d73e4f5f3c416
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-11 05:16:40 +0100

    feat(part4/blog-list): serve blogs from api/blogs
    - allow GET to api/blogs for blogs collection
    - allow POST to api/blogs to add new blogs to server db
    - interface with mongodb cloud db using mongoose api
    - add vs code REST Client tests

---
+++ part4/blog-list/index.js
@@ -0,0 +1,42 @@
+// const http = require("http");
+const express = require("express");
+
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  console.log(request.body);
+  const blog = new Blog(request.body);
+
+  blog.save().then(result => {
+    response.status(201).json(result);
+  });
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
