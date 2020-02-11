c866471357302a8a2580548414c8757a032486ff
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-11 09:30:40 +0100

    dbg(part4/blog-list): log db connection errors

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -8,9 +8,22 @@ const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+console.log("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch(err => {
+    console.log("error connecting to MongoDB:", err.message);
+  });
+
+mongoose.connection.on("error", err => {
+  console.log("connection to MongoDB lost", err.message);
 });
 
 app.use(cors());
