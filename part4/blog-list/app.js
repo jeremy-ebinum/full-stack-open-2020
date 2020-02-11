579ce3f48c606d39733bb777bce6d91621012a0c
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-11 06:18:25 +0100

    refactor(part4/blog-list): modularize blogs routes

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
@@ -15,18 +15,6 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
