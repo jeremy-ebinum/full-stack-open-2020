67e13810530297815757786eaef423623dc2bd94
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-11 13:35:28 +0100

    feat(part4/blog-list): add totalLikes helper
    totalLikes is called on an array of blog objects
    it should return the sum of likes of each blog or 0 on an empty array

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
