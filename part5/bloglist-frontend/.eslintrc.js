284567fe98310d1ffc7e6f5931ebb3476900bb21
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-15 18:36:18 +0100

    wip(part5): initial commit
    - Boostrapped project with create-react-app and added starter code from
      https://github.com/fullstackopen-2019/bloglist-frontend
    - Eslint rules and plugins were also configured

---
+++ part5/bloglist-frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ["react-app", "airbnb", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    quotes: ["error", "double"],
+    semi: 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    eqeqeq: "error",
+    "no-param-reassign": ["error", { props: false }]
+  }
+};
