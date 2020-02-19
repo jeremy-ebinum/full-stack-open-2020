6bfa1ae14e93fb7598f11ced49ec854194cd6ec8
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-19 02:46:53 +0100

    feat(part5): conditionally render Blog details
    By default Blog component shows Title and Author, clicking on the
    component will render full details, including:
     - The link to the Blog
     - The number of likes the Blog has and a like button
     - The name of the user who added the Blog

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Blog = ({ blog }) => (
-  <div className="c-blog">
-    <a
-      className="c-blog__link"
-      href={blog.url}
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      {blog.title}
-    </a>
-    <span className="c-blog__author">by {blog.author}</span>
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleShowDetails = event => {
+    const isBlogLink = event.target.classList.contains("js-blogLink");
+    const isLikeButton = event.target.classList.contains("js-likeButton");
+    if (!isBlogLink && !isLikeButton) {
+      setShowDetails(!showDetails);
+    }
+  };
+
+  const displayBlog = () => {
+    if (!showDetails) {
+      return <span className="c-blog__title">{blog.title}</span>;
+    }
+
+    return (
+      <>
+        <span className="c-blog__title">{blog.title}</span>
+        <span className="c-blog__author"> — {blog.author}</span>
+        <a
+          className="c-blog__link js-blogLink"
+          href={blog.url}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Visit Blog
+        </a>
+        <div className="c-blog__likes">
+          {blog.likes} {blog.likes !== 1 ? "Likes" : "Like"}
+          <div className="c-blog__like-button">
+            <button
+              onClick={event => console.log("LIKED!")}
+              className="c-btn c-btn--info js-likeButton"
+            >
+              Like
+            </button>
+          </div>
+        </div>
+        <span className="c-blog__user">
+          Added by:
+          <FontAwesomeIcon className="c-blog__usericon" icon={faUser} />
+          {blog.user.name}
+        </span>
+      </>
+    );
+  };
+
+  return (
+    <div onClick={event => toggleShowDetails(event)} className="c-blog">
+      {displayBlog()}
+    </div>
+  );
+};
 
 export default Blog;
