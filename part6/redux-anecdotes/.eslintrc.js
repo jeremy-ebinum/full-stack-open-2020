85a0bf333a5cea9d3dcaae5f5ca072bd1c7179b9
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-02-26 22:53:45 +0100

    wip(part6/redux-anecdotes): initial commit

---
+++ part6/redux-anecdotes/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: ["react-app", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: ["jest", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        arrowParens: "always",
+        printWidth: 80,
+        tabWidth: 2,
+        semi: true,
+        singleQuote: false,
+        bracketSpacing: true,
+      },
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "no-param-reassign": ["error", { props: false }],
+    "no-console": 0,
+    // "react/prop-types": 0,
+    "jsx-a11y/label-has-associated-control": [
+      2,
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: ["Input"],
+        assert: "either",
+        depth: 3,
+      },
+    ],
+  },
+};
