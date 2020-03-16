a960df9f2f5ab3a65d19549c7add592f64387c57
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-16 18:34:29 +0100

    vendor,style(part1/anecdotes): update scripts

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-const Heading = props => <h2>{props.text}</h2>;
+const Heading = (props) => <h2>{props.text}</h2>;
 
-const Button = props => <button onClick={props.onClick}>{props.text}</button>;
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
-const Anecdote = props => {
+const Anecdote = (props) => {
   return (
     <>
       {props.anecdote}
@@ -15,7 +16,7 @@ const Anecdote = props => {
   );
 };
 
-const MostVoted = props => {
+const MostVoted = (props) => {
   return (
     <>
       <Heading text="Anecdote with Most Votes" />
@@ -27,12 +28,12 @@ const MostVoted = props => {
   );
 };
 
-const App = props => {
+const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
   const [hasVotes, setHasVotes] = useState(false);
 
-  const getRandomIdx = length => {
+  const getRandomIdx = (length) => {
     return Math.floor(Math.random() * length);
   };
 
@@ -53,7 +54,7 @@ const App = props => {
     setHasVotes(true);
   };
 
-  const handleButtonClick = type => {
+  const handleButtonClick = (type) => {
     switch (type) {
       case "next":
         setNewRandomAnecdote();
@@ -102,7 +103,7 @@ const anecdotes = [
   "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
   "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
