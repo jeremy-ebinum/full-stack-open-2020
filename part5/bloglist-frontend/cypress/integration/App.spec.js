b070fff39fc9c9bbc3dbc170bef1b9050473c540
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-18 10:15:47 +0100

    test(part5/bloglist-frontend): refactor App.spec.js
    closure(d) variables have been refactored to use fixtures and aliases
    some assertions removed in favor of default assertions
    
    updated .gitignore for cypress

--- part5/bloglist-frontend/cypress/integration/App.spec.js
+++ part5/bloglist-frontend/cypress/integration/App.spec.js
@@ -1,27 +1,17 @@
 import { login, createBlog } from "../support/utils";
 
 describe("App ", function() {
-  let user;
-  let validBlog;
-
-  before(function() {
-    user = {
-      name: "Test User",
-      username: "test_username",
-      password: "test_password",
-    };
-
-    validBlog = {
-      title: "Test Blog Title",
-      author: "Test Blog Author",
-      url: "http://site.com",
-    };
-  });
-
   beforeEach(function() {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
 
-    cy.request("POST", "http://localhost:3003/api/users/", user);
+    cy.fixture("users.json").as("users");
+    cy.fixture("blogs.json").as("blogs");
+
+    cy.get("@users").then((users) => {
+      const user = users[0];
+      cy.request("POST", "http://localhost:3003/api/users/", user);
+    });
+
     cy.visit("http://localhost:3000");
   });
 
@@ -31,17 +21,28 @@ describe("App ", function() {
 
   describe("Logging in", function() {
     it("succeeds with correct credentials", function() {
-      login(user.username, user.password);
+      cy.get("@users").then((users) => {
+        const user = users[0];
+        login(user.username, user.password);
 
-      const names = `(${user.username}|${user.name})`;
-      const notificationRegex = new RegExp(`logged|signed (.*)?${names}`, "i");
+        const names = `(${user.username}|${user.name})`;
+        const notificationRegex = new RegExp(
+          `logged|signed (.*)?${names}`,
+          "i"
+        );
 
-      cy.contains(notificationRegex);
-      cy.get("[data-testid='App_blogs']").should("be.visible");
+        cy.contains(notificationRegex);
+        cy.get("[data-testid='App_blogs']").should("be.visible");
+      });
     });
 
     it("fails with wrong credentials", function() {
+      cy.server();
+      cy.route("POST", "/api/login").as("failedLoginReq");
+
       login("spam", "eggs");
+
+      cy.get("@failedLoginReq").should("have.property", "status", 401);
 
       cy.contains(/invalid|missing|wrong/i)
         .parent("[data-testid^='Alert_']")
@@ -54,7 +55,10 @@ describe("App ", function() {
 
   describe("When logged in", function() {
     beforeEach(function() {
-      login(user.username, user.password);
+      cy.get("@users").then((users) => {
+        const user = users[0];
+        login(user.username, user.password);
+      });
     });
 
     it("user can be logged out", function() {
@@ -68,102 +72,120 @@ describe("App ", function() {
       cy.route("/api/blogs").as("initBlogs");
       cy.wait(["@initBlogs"]);
       cy.get("[data-testid='App_showBlogFormBtn']").click();
-      createBlog(validBlog);
+      cy.get("@blogs").then((blogs) => {
+        createBlog(blogs[0]);
+      });
 
       cy.request("GET", "http://localhost:3003/api/testing/blogs").then(
         (res) => {
           const blogs = res.body;
-          expect(blogs).to.have.length(1);
           const blog = blogs[0];
-          expect(blog.title).to.equal(validBlog.title);
-          expect(blog.author).to.equal(validBlog.author);
-          expect(blog.url).to.equal(validBlog.url);
+          expect(blogs).to.have.length(1);
+          cy.get("@blogs").then((blogs) => {
+            const { title, author, url } = blogs[0];
+
+            expect(blog.title).to.equal(title);
+            expect(blog.author).to.equal(author);
+            expect(blog.url).to.equal(url);
+          });
 
           cy.get("[data-testid='BlogList_container']").within(() => {
-            cy.get(`[data-testid="Blog_${blog.id}"]`).should("exist");
+            cy.get(`[data-testid="Blog_${blog.id}"]`);
           });
         }
       );
     });
 
     describe("A created blog", function() {
-      let blogId;
-      let blogLikes;
-
       beforeEach(function() {
         cy.server();
         cy.route("/api/blogs").as("initBlogs");
         cy.wait(["@initBlogs"]);
         cy.get("[data-testid='App_showBlogFormBtn']").click();
-        createBlog(validBlog);
+        cy.get("@blogs").then((blogs) => {
+          createBlog(blogs[0]);
+        });
 
         cy.request("GET", "http://localhost:3003/api/testing/blogs").then(
           (res) => {
-            blogId = res.body[0].id;
-            blogLikes = res.body[0].likes;
+            const createdBlog = {
+              id: res.body[0].id,
+              likes: res.body[0].likes,
+            };
+
+            cy.wrap(createdBlog).as("createdBlog");
           }
         );
       });
 
       it("can be liked", function() {
-        cy.get(`[data-testid='Blog_${blogId}_viewButton']`).click();
-        cy.get(`[data-testid='Blog_${blogId}_likesTxt']`).as("blogLikesTxt");
-        cy.get("@blogLikesTxt").should("contain.text", blogLikes);
-        cy.get(`[data-testid='Blog_${blogId}_likeButton']`).click();
-        cy.get("@blogLikesTxt").should("contain.text", blogLikes + 1);
+        cy.get("@createdBlog").then((blog) => {
+          cy.get(`[data-testid='Blog_${blog.id}_viewButton']`).click();
+          cy.get(`[data-testid='Blog_${blog.id}_likesTxt']`).as("blogLikesTxt");
+          cy.get("@blogLikesTxt").should("contain.text", blog.likes);
+
+          cy.get(`[data-testid='Blog_${blog.id}_likeButton']`).click();
+          cy.get("@blogLikesTxt").should("contain.text", blog.likes + 1);
+        });
       });
 
       it("can be deleted by the user who created it", function() {
-        cy.get(`[data-testid="Blog_${blogId}"]`).should("exist");
-        cy.get(`[data-testid='Blog_${blogId}_viewButton']`).click();
-        cy.get(`[data-testid='Blog_${blogId}_deleteButton']`).click();
-        cy.get(`[data-testid="Blog_${blogId}"]`).should("not.exist");
+        cy.get("@createdBlog").then((blog) => {
+          cy.get(`[data-testid="Blog_${blog.id}"]`);
+
+          cy.get(`[data-testid='Blog_${blog.id}_viewButton']`).click();
+          cy.get(`[data-testid='Blog_${blog.id}_deleteButton']`).click();
+
+          cy.get(`[data-testid="Blog_${blog.id}"]`).should("not.exist");
+        });
       });
 
       it("can not be deleted by other users", function() {
         cy.get("[data-testid='NavBar_logoutBtn']").click();
 
-        let otherUser = {
-          name: "Other Test User",
-          username: "other_test_username",
-          password: "other_test_password",
-        };
+        cy.get("@users").then((users) => {
+          const otherUser = users[1];
+          cy.request("POST", "http://localhost:3003/api/users/", otherUser);
 
-        cy.request("POST", "http://localhost:3003/api/users/", otherUser);
+          cy.request("POST", "http://localhost:3003/api/login/", {
+            username: otherUser.username,
+            password: otherUser.password,
+          }).then((res) => {
+            const { token } = res.body;
 
-        cy.request("POST", "http://localhost:3003/api/login/", {
-          username: otherUser.username,
-          password: otherUser.password,
-        }).then((res) => {
-          const { token } = res.body;
+            cy.wrap(token).as("otherUserToken");
+          });
 
           login(otherUser.username, otherUser.password);
+        });
 
-          cy.server();
-          cy.route("/api/blogs").as("initBlogs");
-          cy.wait(["@initBlogs"]);
+        cy.server();
+        cy.route("/api/blogs").as("initBlogs");
+        cy.wait(["@initBlogs"]);
 
-          cy.get(`[data-testid='Blog_${blogId}']`).within(() => {
-            cy.get(`[data-testid='Blog_${blogId}_viewButton']`).click();
-            cy.get(`[data-testid='Blog_${blogId}_deleteButton']`).should(
+        cy.get("@createdBlog").then((blog) => {
+          cy.get(`[data-testid='Blog_${blog.id}']`).within(() => {
+            cy.get(`[data-testid='Blog_${blog.id}_viewButton']`).click();
+            cy.get(`[data-testid='Blog_${blog.id}_deleteButton']`).should(
               "not.exist"
             );
           });
 
-          const options = {
-            url: `http://localhost:3003/api/blogs/${blogId}`,
-            method: "DELETE",
-            headers: {
-              "User-Agent": "request",
-              Authorization: `Bearer ${token}`,
-            },
-            failOnStatusCode: false,
-          };
+          cy.get("@otherUserToken").then((token) => {
+            const options = {
+              url: `http://localhost:3003/api/blogs/${blog.id}`,
+              method: "DELETE",
+              headers: {
+                "User-Agent": "request",
+                Authorization: `Bearer ${token}`,
+              },
+              failOnStatusCode: false,
+            };
 
-          cy.request(options).then((res) => {
-            expect(res.status).to.eq(403);
-
-            cy.get(`[data-testid='Blog_${blogId}']`).should("exist");
+            cy.request(options).then((res) => {
+              expect(res.status).to.eq(403);
+              cy.get(`[data-testid='Blog_${blog.id}']`);
+            });
           });
         });
       });
@@ -174,56 +196,49 @@ describe("App ", function() {
       cy.route("/api/blogs").as("initBlogs");
       cy.wait(["@initBlogs"]);
 
-      const firstBlog = { ...validBlog, title: "First Blog" };
-      const secondBlog = { ...validBlog, title: "Second Blog" };
-      const thirdBlog = { ...validBlog, title: "Third Blog" };
+      cy.get("@blogs").then((blogs) => {
+        if (blogs.length < 3) throw Error("Not Enough Blogs");
+        const [firstBlog, secondBlog, thirdBlog] = blogs;
 
-      cy.get("[data-testid='App_showBlogFormBtn']").click();
-      createBlog(firstBlog);
-      cy.get("[data-testid='App_showBlogFormBtn']").click();
-      createBlog(secondBlog);
-      cy.get("[data-testid='App_showBlogFormBtn']").click();
-      createBlog(thirdBlog);
+        cy.get("[data-testid='App_showBlogFormBtn']").click();
+        createBlog(firstBlog);
+        cy.get("[data-testid='App_showBlogFormBtn']").click();
+        createBlog(secondBlog);
+        cy.get("[data-testid='App_showBlogFormBtn']").click();
+        createBlog(thirdBlog);
+      });
 
       cy.request("GET", "http://localhost:3003/api/testing/blogs").then(
         (res) => {
           const blogs = res.body;
 
-          let firstBlogId;
-          let secondBlogId;
-          let thirdBlogId;
+          const [blogOne, blogTwo, blogThree] = blogs.map((blog) => {
+            return { id: blog.id, title: blog.title };
+          });
 
-          blogs.reduce((acc, blog) => {
-            if (blog.title === firstBlog.title) {
-              firstBlogId = blog.id;
-            } else if (blog.title === secondBlog.title) {
-              secondBlogId = blog.id;
-            } else {
-              thirdBlogId = blog.id;
-            }
+          cy.get(`[data-testid='Blog_${blogOne.id}_viewButton']`).click();
+          cy.get(`[data-testid='Blog_${blogTwo.id}_viewButton']`).click();
+          cy.get(`[data-testid='Blog_${blogThree.id}_viewButton']`).click();
 
-            return acc;
-          }, []);
-
-          cy.get(`[data-testid='Blog_${firstBlogId}_viewButton']`).click();
-          cy.get(`[data-testid='Blog_${secondBlogId}_viewButton']`).click();
-          cy.get(`[data-testid='Blog_${thirdBlogId}_viewButton']`).click();
-
-          cy.get(`[data-testid='Blog_${secondBlogId}_likeButton']`).click();
-          cy.get(`[data-testid='Blog_${secondBlogId}_likeButton']`).click();
-          cy.get(`[data-testid='Blog_${thirdBlogId}_likeButton']`).click();
+          // blogTwo will have 2 likes, blogThree, 1 like and blogOne, 0 likes
+          cy.get(`[data-testid='Blog_${blogTwo.id}_likeButton']`)
+            .click()
+            .click();
+          cy.get(`[data-testid='Blog_${blogThree.id}_likeButton']`).click();
 
           cy.get(`[data-testid='BlogList_container']`)
             .children(`[data-testid^='Blog_']`)
             .as("blogList");
 
+          // blogTwo should be first
           cy.get("@blogList")
             .first()
-            .should("contain.text", secondBlog.title);
+            .should("contain.text", blogTwo.title);
 
+          // blogOne should be last
           cy.get("@blogList")
             .last()
-            .should("contain.text", firstBlog.title);
+            .should("contain.text", blogOne.title);
         }
       );
     });
