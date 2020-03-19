8b46d6df7a98833887c3259662b23697140b21be
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-19 18:28:59 +0100

    feat(part8/library-backend): add editAuthor mutation

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -154,6 +155,14 @@ const resolvers = {
 
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) return null;
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
