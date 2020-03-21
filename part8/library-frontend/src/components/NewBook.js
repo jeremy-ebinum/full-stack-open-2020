4632f7bed619e6200eccc8ecdaabc9e33a2ba8d9
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-21 17:48:56 +0100

    feat(part8/library-frontend): implement creating books at the /add view

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,34 +1,96 @@
-import React, { useState } from "react";
+import React, { useCallback } from "react";
+import { useMutation } from "@apollo/client";
+import { useUIDSeed } from "react-uid";
+import * as yup from "yup";
+import { useForm, useFieldArray } from "react-hook-form";
 import { Helmet } from "react-helmet-async";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
+import { CREATE_BOOK, GET_ALL_BOOKS, GET_ALL_AUTHORS } from "../queries";
+import useYupValidationResolver from "../hooks/useYupValidationResolver";
 import LinkedNavBar from "./LinkedNavBar";
 
+const validationSchema = yup.object().shape({
+  title: yup.string().required(),
+  author: yup.string().required(),
+  published: yup
+    .number()
+    .typeError("published must be a number")
+    .integer()
+    .required(),
+  genre: yup.string(),
+  genres: yup
+    .array()
+    .of(yup.string())
+    .ensure(),
+});
+
 const NewBook = () => {
-  const [title, setTitle] = useState("");
-  const [author, setAuhtor] = useState("");
-  const [published, setPublished] = useState("");
-  const [genre, setGenre] = useState("");
-  const [genres, setGenres] = useState([]);
+  const uidSeed = useUIDSeed();
+  const validationResolver = useYupValidationResolver(validationSchema);
+  const {
+    control,
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    errors,
+    formState,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+    defaultValues: {
+      title: "",
+      author: "",
+      published: "",
+      genre: "",
+      genres: [],
+    },
+    validationResolver,
+  });
 
-  const submit = async (event) => {
-    event.preventDefault();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "genres",
+  });
 
-    console.log("add book...");
+  const { touched, isSubmitting } = formState;
 
-    setTitle("");
-    setPublished("");
-    setAuhtor("");
-    setGenres([]);
-    setGenre("");
-  };
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: GET_ALL_BOOKS }, { query: GET_ALL_AUTHORS }],
+  });
 
-  const addGenre = () => {
-    setGenres(genres.concat(genre));
-    setGenre("");
-  };
+  const addGenre = useCallback(() => {
+    const { genre } = getValues();
+    if (genre) {
+      append(genre);
+      setValue("genre", "");
+    }
+  }, [setValue, getValues, append]);
+
+  const handleGenreKeyPress = useCallback(
+    (event) => {
+      if (event.charCode === 13) {
+        event.preventDefault();
+        addGenre();
+      }
+    },
+    [addGenre]
+  );
+
+  const addBook = useCallback(
+    async (data, event) => {
+      const { title, author, published, genres } = data;
+      await createBook({ variables: { title, author, published, genres } });
+      reset();
+    },
+    [createBook, reset]
+  );
 
   return (
     <>
@@ -37,45 +99,134 @@ const NewBook = () => {
       </Helmet>
       <LinkedNavBar />
       <Container>
-        <Row>
-          <Col>
-            <form onSubmit={submit}>
-              <div>
-                title
-                <input
-                  value={title}
-                  onChange={({ target }) => setTitle(target.value)}
+        <Form onSubmit={handleSubmit(addBook)} className="mt-4">
+          <h2 className="text-success mb-3">Add a new Book</h2>
+
+          <Form.Group as={Form.Row} controlId={uidSeed("title")}>
+            <Form.Label column sm={2} md={1}>
+              Title
+            </Form.Label>
+            <Col sm={10} md={6}>
+              <Form.Control
+                ref={register}
+                type="text"
+                name="title"
+                placeholder="Book Title"
+                isValid={touched.title && !errors.title}
+                isInvalid={!!errors.title}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.title && errors.title.message}
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Form.Row} controlId={uidSeed("author")}>
+            <Form.Label column sm={2} md={1}>
+              Author
+            </Form.Label>
+            <Col sm={10} md={6}>
+              <Form.Control
+                ref={register}
+                type="text"
+                name="author"
+                placeholder="Book Author"
+                isValid={touched.author && !errors.author}
+                isInvalid={!!errors.author}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.author && errors.author.message}
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Form.Row} controlId={uidSeed("published")}>
+            <Form.Label column sm={2} md={1}>
+              Published
+            </Form.Label>
+            <Col sm={10} md={6}>
+              <Form.Control
+                ref={register}
+                type="number"
+                name="published"
+                placeholder="Publication Year"
+                isValid={touched.published && !errors.published}
+                isInvalid={!!errors.published}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.published && errors.published.message}
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Form.Row} controlId={uidSeed("genre")}>
+            <Form.Label column sm={2} md={1}>
+              Genres
+            </Form.Label>
+            <Col sm={10} md={6}>
+              <InputGroup>
+                <Form.Control
+                  ref={register}
+                  onKeyPress={handleGenreKeyPress}
+                  type="text"
+                  name="genre"
+                  placeholder="Book Genre"
+                  aria-label="Add a Genre for the Book"
+                  aria-describedby={uidSeed("genre")}
                 />
-              </div>
+
+                <InputGroup.Append>
+                  <Button type="button" variant="primary" onClick={addGenre}>
+                    Add
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+              <Form.Text className="text-muted">
+                Enter a genre and click the button to add to the list of genres
+              </Form.Text>
+
               <div>
-                author
-                <input
-                  value={author}
-                  onChange={({ target }) => setAuhtor(target.value)}
-                />
+                {fields.map((genre, index) => (
+                  <div
+                    key={genre.id}
+                    className="d-inline-block border px-3 py-2 mr-2 mb-2"
+                  >
+                    <input
+                      className="d-none"
+                      ref={register}
+                      name={`genres[${index}]`}
+                    ></input>
+                    <button
+                      type="button"
+                      className="close ml-3"
+                      aria-label="Remove Genre"
+                      onClick={() => remove(index)}
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    <span>{genre.value}</span>
+                  </div>
+                ))}
               </div>
-              <div>
-                published
-                <input
-                  type="number"
-                  value={published}
-                  onChange={({ target }) => setPublished(target.value)}
-                />
-              </div>
-              <div>
-                <input
-                  value={genre}
-                  onChange={({ target }) => setGenre(target.value)}
-                />
-                <button onClick={addGenre} type="button">
-                  add genre
-                </button>
-              </div>
-              <div>genres: {genres.join(" ")}</div>
-              <button type="submit">create book</button>
-            </form>
-          </Col>
-        </Row>
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row}>
+            <Col sm={{ offset: 2 }} md={{ offset: 1, span: 6 }}>
+              <Button
+                variant="success"
+                type="submit"
+                size="lg"
+                block
+                disabled={isSubmitting}
+              >
+                Create Book
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
       </Container>
     </>
   );
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -22,3 +22,25 @@ export const GET_ALL_BOOKS = gql`
     }
   }
 `;
+
+export const CREATE_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+      id
+    }
+  }
+`;
