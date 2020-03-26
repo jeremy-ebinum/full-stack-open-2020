76d3f7a374ed0d7aee12e54e00753e391c04018e
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-27 00:47:07 +0100

    chore(part8/library-backend): config eslint

---
+++ part8/library-backend/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        arrowParens: "always",
+        printWidth: 80,
+        tabWidth: 2,
+        semi: true,
+        singleQuote: false,
+        bracketSpacing: true,
+      },
+    ],
+    "no-console": 0,
+    "no-param-reassign": ["error", { props: false }],
+    "no-underscore-dangle": ["error", { allow: ["_id", "__v"] }],
+  },
+};
