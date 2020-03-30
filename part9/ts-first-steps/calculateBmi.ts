1c96d685c70985542c1ea253d231992bcadc67df
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-03-30 22:27:37 +0100

    feat(part9/ts-first-steps): add cmd line support for calculateBmi
    The function is now run with:
      "npm run calculateBmi height(cm) weight(cm)"

--- part9/ts-first-steps/calculateBmi.ts
+++ part9/ts-first-steps/calculateBmi.ts
@@ -11,4 +11,29 @@ const calculateBmi = (height: number, weight: number): string => {
   if (bmi >= 40) return "Obese Class III (Very severely obese)";
 };
 
-console.log(calculateBmi(180, 74));
+interface ParsedBmiArgs {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): ParsedBmiArgs => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  console.log("An error has occured: ", e.message);
+  console.log("USAGE: npm run calculateBmi height(cm) weight(kg)");
+}
