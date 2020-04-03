54e7f79cdac277bac0e44897aacac1ffb2574289
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-04-03 23:43:52 +0100

    wip(part9/typed-courseinfo): initial commit
    Setup eslint and add initial project code

---
+++ part9/typed-courseinfo/src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const App: React.FC = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  );
+};
+
+export default App
